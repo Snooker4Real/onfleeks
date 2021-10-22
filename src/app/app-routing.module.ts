8a5a4234620257c01d9c517199ed27a41c5fc8a5
Cindano <cindanojonathan@icloud.com>
2021-10-23 00:54:32 +0200

    login done!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AuthGuard} from "./guards/auth/auth.guard";
+import {TvShowViewComponent} from "./views/tv-shows-view/tv-show-view.component";
+import {NewTvShowViewComponent} from "./views/new-tv-show-view/new-tv-show-view.component";
+import {SingleTvShowViewComponent} from "./views/single-tv-show-view/single-tv-show-view.component";
+import {EditTvShowViewComponent} from "./views/edit-tv-show-view/edit-tv-show-view.component";
+import {AuthViewComponent} from "./views/auth-view/auth-view.component";
+import {ErrorViewComponent} from "./views/error-view/error-view.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', canActivate: [AuthGuard], component: TvShowViewComponent},
+  {path: 'home', canActivate: [AuthGuard], component: TvShowViewComponent},
+  {path: 'tv-shows/new', canActivate: [AuthGuard], component: NewTvShowViewComponent},
+  {path: 'tv-shows/:id', canActivate: [AuthGuard], component: SingleTvShowViewComponent},
+  {path: 'tv-shows/:id/edit', canActivate: [AuthGuard], component: EditTvShowViewComponent},
+  {path: 'auth', component: AuthViewComponent},
+  {path: 'not-found', component: ErrorViewComponent},
+  {path: '**', redirectTo: 'not-found'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthViewComponent } from './views/auth-view/auth-view.component';
+import { TvShowViewComponent } from './views/tv-shows-view/tv-show-view.component';
+import { EditTvShowViewComponent } from './views/edit-tv-show-view/edit-tv-show-view.component';
+import { ErrorViewComponent } from './views/error-view/error-view.component';
+import { NewTvShowViewComponent } from './views/new-tv-show-view/new-tv-show-view.component';
+import { SingleTvShowViewComponent } from './views/single-tv-show-view/single-tv-show-view.component';
+import { TvShowFormComponent } from './components/tv-show-form/tv-show-form.component';
+import { ErrorsFormComponent } from './components/errors-form/errors-form.component';
+import { HeaderComponent } from './components/header/header.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthViewComponent,
+    TvShowViewComponent,
+    EditTvShowViewComponent,
+    ErrorViewComponent,
+    NewTvShowViewComponent,
+    SingleTvShowViewComponent,
+    TvShowFormComponent,
+    ErrorsFormComponent,
+    HeaderComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
